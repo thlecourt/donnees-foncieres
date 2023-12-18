d3ceb64f14f535a77b526ce885598d469d71cb08
thlecourt <118343266+thlecourt@users.noreply.github.com>
2023-12-18 16:49:33 +0100

    Update harmonisation-structure-multimilllesime.sql

--- Fichiers-Fonciers/harmonisation-structure-multimilllesime.sql
+++ Fichiers-Fonciers/harmonisation-structure-multimilllesime.sql
@@ -1,1 +1,50 @@
+--- Ce script sert à donner aux millésimes des Fichiers Fonciers antérieurs à 2018 la même structure que les millésimes suivants :  1 schéma "national" + 1 schéma "départemental" par millésime
+--- NB : cet exemple fonctionne pour le millésime 2014. Remplacer toutes les mentions de '14' par le millésime souhaité
 
+--- CREATION DES SCHEMAS FINAUX
+CREATE SCHEMA IF NOT EXISTS ff_2014;
+CREATE SCHEMA IF NOT EXISTS ff_2014_dep;
+CREATE SCHEMA IF NOT EXISTS ffta_2014;
+CREATE SCHEMA IF NOT EXISTS ffta_2014_dep;
+
+--- CHANGEMENT DE SCHEMA DES TABLES DEPARTEMENTALES
+--- Exécuter manuellement les requêtes SQL générées par le code suivant
+SELECT 'ALTER TABLE '||table_schema||'.'||table_name||' SET SCHEMA ff_2014_dep;' 
+   FROM information_schema.tables 
+   WHERE table_type = 'BASE TABLE' AND table_schema LIKE 'ff_d%14'
+UNION
+SELECT 'ALTER TABLE '||table_schema||'.'||table_name||' SET SCHEMA ffta_2014_dep;' 
+   FROM information_schema.tables 
+   WHERE table_type = 'BASE TABLE' AND table_schema LIKE 'ffta_d%14';
+
+--- SUPPRESSION DES SCHEMAS VIDES ET INUTILISES
+--- Exécuter manuellement les requêtes SQL générées par le code suivant
+SELECT 'DROP SCHEMA '||schema_name||' CASCADE;'
+FROM information_schema.schemata
+WHERE schema_name ILIKE 'ff_d%14'
+UNION
+SELECT 'DROP SCHEMA '||schema_name||' CASCADE;'
+FROM information_schema.schemata
+WHERE schema_name ILIKE 'ffta_d%14';
+
+--- STRUCTURATION D'UNE TABLE NATIONALE
+--- Exécuter manuellement les requêtes SQL générées par le code suivant
+SELECT 'CREATE TABLE ff_2014.fftp'||RIGHT(table_name,LENGTH(table_name)-3)||' (LIKE '||table_schema||'.'||table_name||' INCLUDING DEFAULTS);'
+FROM information_schema.tables 
+   WHERE table_type = 'BASE TABLE' AND table_schema LIKE 'ff_%14_dep' AND table_name LIKE 'd01%'
+UNION
+SELECT 'CREATE TABLE ffta_2014.ffta'||RIGHT(table_name,LENGTH(table_name)-3)||' (LIKE '||table_schema||'.'||table_name||' INCLUDING DEFAULTS);'
+FROM information_schema.tables 
+   WHERE table_type = 'BASE TABLE' AND table_schema LIKE 'ffta_%14_dep' AND table_name LIKE 'd01%';
+
+--- IMPLEMENTATION DE L'HERITAGE
+--- Exécuter manuellement les requêtes SQL générées par le code suivant
+SELECT 'ALTER TABLE ff_2014_dep.'||table_name||' INHERIT ff_2014.'||'fftp'||RIGHT(table_name,LENGTH(table_name)-3)||';' 
+   FROM information_schema.tables 
+   WHERE table_type = 'BASE TABLE'
+   	AND table_schema LIKE 'ff_2014_dep'
+UNION
+SELECT 'ALTER TABLE ffta_2014_dep.'||table_name||' INHERIT ffta_2014.'||'ffta'||RIGHT(table_name,LENGTH(table_name)-3)||';' 
+   FROM information_schema.tables 
+   WHERE table_type = 'BASE TABLE'
+   	AND table_schema LIKE 'ffta_2014_dep';
