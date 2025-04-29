2730d6a8892ffe0df0b51b3e9b2ec0fa7cb1bac9
thlecourt <118343266+thlecourt@users.noreply.github.com>
2025-04-29 16:07:15 +0200

    Update attribution_niveaux_propriete_des_parcelles_communales.sql

--- Fichiers-Fonciers/attribution_niveaux_propriete_des_parcelles_communales.sql
+++ Fichiers-Fonciers/attribution_niveaux_propriete_des_parcelles_communales.sql
@@ -4,13 +4,15 @@ DECLARE
 	millesime text;
 	millesime_short text;
 	table_name text;
+	table_communaux text;
 BEGIN
 	RAISE NOTICE 'Traitement multi-millésime pour affectation des propriétaires (inter)communaux démarré';
-	FOREACH millesime IN ARRAY ARRAY['2009','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021']
+	FOREACH millesime IN ARRAY ARRAY['2021','2009','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020']
 	LOOP
 		RAISE NOTICE 'Traitement du millésime % en cours',millesime;
 		millesime_short = RIGHT(millesime,2);
 		table_name = 'public'||millesime_short;
+		table_communaux = 'com'||millesime_short;
 
 		RAISE NOTICE 'identification des parcelles communales';
 
@@ -19,10 +21,9 @@ BEGIN
 			ALTER TABLE nat.%1$s DROP COLUMN IF EXISTS cominterco CASCADE;
 			ALTER TABLE nat.%1$s ADD COLUMN cominterco bool;
 			ALTER TABLE nat.%1$s DROP COLUMN IF EXISTS com_niv CASCADE;
-			ALTER TABLE nat.%1$s ADD COLUMN com_niv int;
-			
+			ALTER TABLE nat.%1$s ADD COLUMN com_niv int;			
 					
-			--cominterco(3m32)
+			--cominterco
 			UPDATE nat.%1$s SET cominterco =
 				CASE
 					WHEN True = any(a_prop_cominterco) THEN True
@@ -34,118 +35,35 @@ BEGIN
 		);
 		COMMIT;
 
-		
-		---UNIQUEMENT PROPRIETAIRES COMMUNAUX
-		RAISE NOTICE 'Au moins 1 non-générique';
-		---Au moins 1 non-générique
-
+		RAISE NOTICE 'attribution des niveaux de propriété';
 		EXECUTE format(
 			$$
 			UPDATE nat.%1$s
 			SET com_niv =
 				CASE
-					WHEN '7' = any(a_prop_com_niv) THEN 7---communaux
-					WHEN '6' = any(a_prop_com_niv) THEN 6---HLM
-					WHEN '5' = any(a_prop_com_niv) THEN 5---Services divers
-					WHEN '4' = any(a_prop_com_niv) THEN 4---Portage foncier
-					WHEN '3' = any(a_prop_com_niv) THEN 3---Aménagement
+					WHEN '7' = any(a_prop_com_niv)
+						OR '7' = any(a_ges_com_niv)
+						THEN 7---communaux
+					WHEN '4' = any(a_prop_com_niv)
+						OR '4' = any(a_ges_com_niv)
+						THEN 4---Portage foncier
+					WHEN '3' = any(a_prop_com_niv)
+						OR '3' = any(a_ges_com_niv)
+						THEN 3---Aménagement
+					WHEN '6' = any(a_prop_com_niv)
+						OR '6' = any(a_ges_com_niv)
+						THEN 6---HLM
+					WHEN '5' = any(a_prop_com_niv)
+						OR '5' = any(a_ges_com_niv)
+						OR ARRAY[FALSE] <@ a_prop_cominterco IS TRUE
+						OR ARRAY[FALSE] <@ a_ges_cominterco IS TRUE
+						THEN 5---Services divers
+					WHEN '2' = any(a_prop_com_niv)
+						OR '2' = any(a_ges_com_niv)
+						THEN 2---Interco
+					ELSE 1--Communes				
 				END
 			WHERE cominterco IS TRUE
-				AND ARRAY[FALSE] <@ a_prop_cominterco IS FALSE;
-			$$,
-			table_name
-		);
-		COMMIT;
-
-		---uniquement générique / pas de gestionnaire
-		RAISE NOTICE 'pas de gestionnaire';
-		EXECUTE format(
-			$$
-			UPDATE nat.%1$s
-			SET com_niv =
-				CASE
-					WHEN '1' = any(a_prop_com_niv) THEN 1---Communes
-					ELSE 2---Intercommunalités
-				END
-			WHERE cominterco IS TRUE
-				AND ARRAY[FALSE] <@ a_prop_cominterco IS FALSE
-				AND com_niv IS NULL
-				AND a_ges_com_niv IS NULL;
-			$$,
-			table_name
-		);
-		COMMIT;
-
-		---uniquement générique / avec gestionnaire(s)
-		--HLM (9s)
-		RAISE NOTICE 'avec gestionnaire';
-		EXECUTE format(
-			$$
-			UPDATE nat.%1$s
-			SET com_niv = 6
-			WHERE cominterco IS TRUE
-				AND ARRAY[FALSE] <@ a_prop_cominterco IS FALSE
-				AND com_niv IS NULL
-				AND 'F1a' = any(a_ges_cod_prop);
-
-			--Gestionnaire communal (6s)
-			UPDATE nat.%1$s
-			SET com_niv =
-				CASE
-					WHEN '7' = any(a_ges_com_niv) THEN 7---Communaux 
-					WHEN '6' = any(a_ges_com_niv) THEN 6---HLM
-					WHEN '5' = any(a_ges_com_niv) THEN 5---Services
-					WHEN '4' = any(a_ges_com_niv) THEN 4---Portage foncier
-					WHEN '3' = any(a_ges_com_niv) THEN 3---Aménagement
-					WHEN '2' = any(a_ges_com_niv) THEN 2---Interco
-					ELSE 1---Commune
-				END
-			WHERE cominterco IS TRUE
-				AND ARRAY[FALSE] <@ a_prop_cominterco IS FALSE
-				AND com_niv IS NULL
-				AND 'True' = any(a_ges_cominterco);
-
-			--Gestionnaire non-communal(5s)
-			UPDATE nat.%1$s
-			SET com_niv = 5
-			WHERE cominterco IS TRUE
-				AND ARRAY[FALSE] <@ a_prop_cominterco IS FALSE
-				AND com_niv IS NULL
-				AND 'True' != any(a_ges_cominterco);
-
-			--- PROPRIETAIRES COMMUNAUX ET NON-COMMUNAUX
-			--Au moins 1 propriétaire HLM (5s)
-			UPDATE nat.%1$s
-			SET com_niv = 6
-			WHERE cominterco IS TRUE
-				AND ARRAY[FALSE] <@ a_prop_cominterco IS TRUE
-				AND 'F1a' = any(a_prop_cod_prop);
-
-			--Au moins 1 propriétaire privé (5s)
-			UPDATE nat.%1$s
-			SET com_niv = 5---Partagée avec propriété privée
-			WHERE cominterco IS TRUE
-				AND ARRAY[FALSE] <@ a_prop_cominterco IS TRUE
-				AND com_niv IS NULL
-				AND ARRAY[FALSE] <@ a_prop_public IS TRUE;
-
-			---Sans propriétaire privé
-			--pas de gestionnaire ou gestionnaire non-hlm (5s)
-			UPDATE nat.%1$s
-			SET com_niv = 5---Partagée avec propriété publique
-			WHERE cominterco IS TRUE
-				AND ARRAY[FALSE] <@ a_prop_cominterco IS TRUE
-				AND com_niv IS NULL
-				AND (a_ges_com_niv IS NULL
-					 OR 'F1a' != any(a_ges_cod_prop));
-
-			--gestionnaire hlm (5s)
-			UPDATE nat.%1$s
-			SET com_niv = 6
-			WHERE cominterco IS TRUE
-				AND ARRAY[FALSE] <@ a_prop_cominterco IS TRUE
-				AND com_niv IS NULL
-				AND 'F1a' = any(a_ges_cod_prop);
 			$$,
 			table_name
 		);
