f97484353a7bdb42283d98d601f1d354bc7e4cf9
thlecourt <118343266+thlecourt@users.noreply.github.com>
2025-07-04 14:54:17 +0200

    Update harmonisation-structure-multimilllesime.sql

--- Fichiers-Fonciers/harmonisation-structure-multimilllesime.sql
+++ Fichiers-Fonciers/harmonisation-structure-multimilllesime.sql
@@ -1,31 +1,5 @@
 --- Ce script sert à donner aux millésimes des Fichiers Fonciers antérieurs à 2018 la même structure que les millésimes suivants :  1 schéma "national" + 1 schéma "départemental" par millésime
---- NB : cet exemple fonctionne pour le millésime 2014. Remplacer toutes les mentions de '14' par le millésime souhaité
-
---- CREATION DES SCHEMAS FINAUX
-CREATE SCHEMA IF NOT EXISTS ff_2014;
-CREATE SCHEMA IF NOT EXISTS ff_2014_dep;
-CREATE SCHEMA IF NOT EXISTS ffta_2014;
-CREATE SCHEMA IF NOT EXISTS ffta_2014_dep;
-
---- CHANGEMENT DE SCHEMA DES TABLES DEPARTEMENTALES
---- Exécuter manuellement les requêtes SQL générées par le code suivant
-SELECT 'ALTER TABLE '||table_schema||'.'||table_name||' SET SCHEMA ff_2014_dep;' 
-   FROM information_schema.tables 
-   WHERE table_type = 'BASE TABLE' AND table_schema LIKE 'ff_d%14'
-UNION
-SELECT 'ALTER TABLE '||table_schema||'.'||table_name||' SET SCHEMA ffta_2014_dep;' 
-   FROM information_schema.tables 
-   WHERE table_type = 'BASE TABLE' AND table_schema LIKE 'ffta_d%14';
-
---- SUPPRESSION DES SCHEMAS VIDES ET INUTILISES
---- Exécuter manuellement les requêtes SQL générées par le code suivant
-SELECT 'DROP SCHEMA '||schema_name||' CASCADE;'
-FROM information_schema.schemata
-WHERE schema_name ILIKE 'ff_d%14'
-UNION
-SELECT 'DROP SCHEMA '||schema_name||' CASCADE;'
-FROM information_schema.schemata
-WHERE schema_name ILIKE 'ffta_d%14';
+--- NB : cet exemple fonctionne pour le millésime 2016. Remplacer toutes les mentions de '16' par le millésime souhaité
 
 --- REPARATION DES GEOMETRIES PONCTUELLES DE LA TABLE DES PARCELLES DU DEPARTEMENT 95 MILLESIME 2016
 --- /!\ Cette étape n'est nécessaire que pour le millésime 2016 et le département 95
@@ -40,24 +14,51 @@ RENAME COLUMN geomloc2 TO geomloc;
 ALTER TABLE ff_2016.d95_2016_pnb10_parcelle
 ALTER COLUMN geomloc TYPE geometry(MultiPoint,2154);
 
+
+--- CREATION DES SCHEMAS FINAUX
+CREATE SCHEMA IF NOT EXISTS ff_2016;
+CREATE SCHEMA IF NOT EXISTS ff_2016_dep;
+CREATE SCHEMA IF NOT EXISTS ffta_2016;
+CREATE SCHEMA IF NOT EXISTS ffta_2016_dep;
+
+--- CHANGEMENT DE SCHEMA DES TABLES DEPARTEMENTALES
+--- Exécuter manuellement les requêtes SQL générées par le code suivant
+SELECT 'ALTER TABLE '||table_schema||'.'||table_name||' SET SCHEMA ff_2016_dep;' 
+   FROM information_schema.tables 
+   WHERE table_type = 'BASE TABLE' AND table_schema LIKE 'ff_d%16'
+UNION
+SELECT 'ALTER TABLE '||table_schema||'.'||table_name||' SET SCHEMA ffta_2016_dep;' 
+   FROM information_schema.tables 
+   WHERE table_type = 'BASE TABLE' AND table_schema LIKE 'ffta_d%16';
+
+--- SUPPRESSION DES SCHEMAS VIDES ET INUTILISES
+--- Exécuter manuellement les requêtes SQL générées par le code suivant
+SELECT 'DROP SCHEMA '||schema_name||' CASCADE;'
+FROM information_schema.schemata
+WHERE schema_name ILIKE 'ff_d%16'
+UNION
+SELECT 'DROP SCHEMA '||schema_name||' CASCADE;'
+FROM information_schema.schemata
+WHERE schema_name ILIKE 'ffta_d%16';
+
 --- STRUCTURATION D'UNE TABLE NATIONALE
 --- Exécuter manuellement les requêtes SQL générées par le code suivant
-SELECT 'CREATE TABLE ff_2014.fftp'||RIGHT(table_name,LENGTH(table_name)-3)||' (LIKE '||table_schema||'.'||table_name||' INCLUDING DEFAULTS);'
+SELECT 'CREATE TABLE ff_2016.fftp'||RIGHT(table_name,LENGTH(table_name)-3)||' (LIKE '||table_schema||'.'||table_name||' INCLUDING DEFAULTS);'
 FROM information_schema.tables 
-   WHERE table_type = 'BASE TABLE' AND table_schema LIKE 'ff_%14_dep' AND table_name LIKE 'd01%'
+   WHERE table_type = 'BASE TABLE' AND table_schema LIKE 'ff_%16_dep' AND table_name LIKE 'd01%'
 UNION
-SELECT 'CREATE TABLE ffta_2014.ffta'||RIGHT(table_name,LENGTH(table_name)-3)||' (LIKE '||table_schema||'.'||table_name||' INCLUDING DEFAULTS);'
+SELECT 'CREATE TABLE ffta_2016.ffta'||RIGHT(table_name,LENGTH(table_name)-3)||' (LIKE '||table_schema||'.'||table_name||' INCLUDING DEFAULTS);'
 FROM information_schema.tables 
-   WHERE table_type = 'BASE TABLE' AND table_schema LIKE 'ffta_%14_dep' AND table_name LIKE 'd01%';
+   WHERE table_type = 'BASE TABLE' AND table_schema LIKE 'ffta_%16_dep' AND table_name LIKE 'd01%';
 
 --- IMPLEMENTATION DE L'HERITAGE
 --- Exécuter manuellement les requêtes SQL générées par le code suivant
-SELECT 'ALTER TABLE ff_2014_dep.'||table_name||' INHERIT ff_2014.'||'fftp'||RIGHT(table_name,LENGTH(table_name)-3)||';' 
+SELECT 'ALTER TABLE ff_2016_dep.'||table_name||' INHERIT ff_2016.'||'fftp'||RIGHT(table_name,LENGTH(table_name)-3)||';' 
    FROM information_schema.tables 
    WHERE table_type = 'BASE TABLE'
-   	AND table_schema LIKE 'ff_2014_dep'
+   	AND table_schema LIKE 'ff_2016_dep'
 UNION
-SELECT 'ALTER TABLE ffta_2014_dep.'||table_name||' INHERIT ffta_2014.'||'ffta'||RIGHT(table_name,LENGTH(table_name)-3)||';' 
+SELECT 'ALTER TABLE ffta_2016_dep.'||table_name||' INHERIT ffta_2016.'||'ffta'||RIGHT(table_name,LENGTH(table_name)-3)||';' 
    FROM information_schema.tables 
    WHERE table_type = 'BASE TABLE'
-   	AND table_schema LIKE 'ffta_2014_dep';
+   	AND table_schema LIKE 'ffta_2016_dep';
